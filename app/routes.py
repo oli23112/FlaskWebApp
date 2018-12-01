7fed047f8f90bd39a0fccfe699fd60f9d3141529
polieter <michalkwasniewicz@gmail.com>
2018-12-01 13:42:22 +0100

    dodano galerie

--- app/routes.py
+++ app/routes.py
@@ -6,7 +6,6 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def getUser(name):
-    return render_template('user.html', name=name)
-
+@app.route('/galeria')
+def getGalery():
+    return render_template("galeria.html")
